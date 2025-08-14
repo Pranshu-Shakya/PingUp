2b78060ecf275303ffddd88ef463bbe3db1abd5e
Pranshu Shakya <pranshushakya2027@gmail.com>
2025-08-14 14:13:04 +0530

    initial commit

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom';
+
+const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
+    const navigate = useNavigate();
+  return (
+    <div className={`w-60 xl:w-72 bg-white border-r border-gray-200 flex flex-col justify-between items-center max-sm:absolute top-0 bottom-0 z-20 ${sidebarOpen ? 'translate-x-0' : 'max-sm'} transition-all duration-300 ease-in-out`}>
+        <div className='w-full'>
+            <img onClick={() => navigate('/')} src={assets.logo} className='w-24 ml-7 my-2 cursor-pointer' alt="" />
+            <hr className='border-gray-300 mb-8' />
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
