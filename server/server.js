07a8f5a498232cfe53542fcd307c564f04bc7dfb
Pranshu Shakya <pranshushakya2027@gmail.com>
2025-08-22 18:11:50 +0530

    user controllers

--- server/server.js
+++ server/server.js
@@ -3,7 +3,9 @@ import cors from "cors";
 import connectDB from "./configs/db.js";
 import { inngest, functions } from "./inngest/index.js";
 import { serve } from "inngest/express";
+import { clerkMiddleware } from "@clerk/express";
 import "dotenv/config";
+import userRouter from "./routes/user.route.js";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -12,12 +14,14 @@ await connectDB();
 
 app.use(express.json());
 app.use(cors());
+app.use(clerkMiddleware());
 
 app.get("/", (req, res) => {
 	res.send("PingUp Server is running!");
 });
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/user", userRouter);
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
