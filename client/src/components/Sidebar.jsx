8682d675849d04b089673edd9a4b3ffe35503c8b
Pranshu Shakya <pranshushakya2027@gmail.com>
2025-08-31 20:49:19 +0530

    Fix sidebar translation logic for open state

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -13,7 +13,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
 	return (
 		<div
 			className={`w-60 xl:w-72 bg-white border-r border-gray-200 flex flex-col justify-between items-center max-sm:absolute top-0 bottom-0 z-20 ${
-				sidebarOpen ? "translate-x-0" : "max-sm"
+				sidebarOpen ? "translate-x-0" : "-translate-x-full"
 			} transition-all duration-300 ease-in-out`}
 		>
 			<div className="w-full">
